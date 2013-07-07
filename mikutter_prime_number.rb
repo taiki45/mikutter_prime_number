3a46363f903e6b06e4eb9a40b057f92c1116c5e4
Taiki ONO <taiks.4559@gmail.com>
2013-07-07 15:07:06 +0900

    init

---
+++ mikutter_prime_number.rb
@@ -0,0 +1,23 @@
+require 'prime'
+
+Plugin.create :mikutter_prime_number do
+  def has_prime?(msg)
+    if msg.to_s =~ /(\d+)/
+      Prime.prime?($1.to_i) ? $1 : false
+    end
+  end
+
+  def tell(msg)
+    ::Plugin.call(:update, nil, [Message.new(message: msg, system: true)])
+  end
+
+  on_update do |service, msgs|
+    msgs.each do |msg|
+      if num = has_prime?(msg)
+        link = "http://twitter.com/#{msg.user}/status/#{msg.id}"
+        Service.primary.post message: "ピピーッ 素数 #{num} を発見しました！ #{link}"
+      end
+    end
+    [service, msgs]
+  end
+end
---
+++ spec
@@ -0,0 +1,9 @@
+---
+slug: :mikutter_prime_number
+depends:
+  mikutter: 0.2.2.1264
+  plugin: []
+version: '1.0'
+author: taiki45
+name: mikutter_prime_number
+description: "Find prime numbers"

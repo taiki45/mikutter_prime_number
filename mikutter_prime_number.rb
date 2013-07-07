dc8c03eaa94ac0d5f8c4baf5fde99d04edc89f31
Taiki ONO <taiks.4559@gmail.com>
2013-07-07 15:29:55 +0900

    exclude tweets from prime number plugin

--- mikutter_prime_number.rb
+++ mikutter_prime_number.rb
@@ -2,13 +2,17 @@ require 'prime'
 
 Plugin.create :mikutter_prime_number do
   def has_prime?(msg)
-    if msg.to_s =~ /(\d+)/
+    if !from_prime_plugin?(msg) && msg.to_s =~ /(\d+)/
       Prime.prime?($1.to_i) ? $1 : false
     end
   end
 
+  def from_prime_plugin?(msg)
+    msg.to_s =~ /ピピーッ/u
+  end
+
   on_update do |service, msgs|
-    msgs.each do |msg|
+    msgs.select {|m| !m.from_me? }.each do |msg|
       if num = has_prime?(msg)
         link = "http://twitter.com/#{msg.user}/status/#{msg.id}"
         Service.primary.post message: "ピピーッ 素数 #{num} を発見しました！ #{link}"
